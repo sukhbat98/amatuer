7f0789068d4aec2782577718a3e1c37a02c5597d
sukhbat98 <svhbat98@gmail.com>
2023-08-15 18:27:46 +0800

    Webpack-ийн хичээл дууссан

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,42 @@
 const path = require("path")
 const postCSSPlugins = [
-    require('postcss-simple-vars'),
-    require('postcss-nested'),
-    require("autoprefixer"),
+  require("postcss-import"),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require("autoprefixer"),
 ]
 
 module.exports = {
-    mode: "development",
-    entry: "./app/assets/scripts/App.js",
-    output: {
-        filename: "bundled.js",
-        path: path.resolve(__dirname, "app"),
+  mode: "development",
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'app'),
     },
-    watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions:{
-                                plugins: postCSSPlugins
-                            }
-                        }
-                    }
-                ],
-            },
+    hot: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions:{
+                plugins: postCSSPlugins
+              }
+            }
+          }
         ],
-    },
+      },
+    ],
+  },
 }
