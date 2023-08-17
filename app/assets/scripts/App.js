a185c295bbd92af36bace39fba575923e983136b
sukhbat98 <svhbat98@gmail.com>
2023-08-17 11:44:16 +0800

    reveal feature-ийг бүрэн хийж дуусгав

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,13 @@
 import '../styles/styles.css'
 
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 
 
-let mobileMenu = new MobileMenu()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
     module.hot.accept()
