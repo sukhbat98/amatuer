0961045663fb075e14561fec8dfc5a3484c5659a
sukhbat98 <svhbat98@gmail.com>
2023-09-19 20:24:37 +0800

    first commit

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,39 +5,39 @@ import 'lazysizes'
 
 import '../styles/styles.css';
 
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader';
-import ClientArea from './modules/ClientArea';
+// import MobileMenu from './modules/MobileMenu';
+// import RevealOnScroll from './modules/RevealOnScroll';
+// import StickyHeader from './modules/StickyHeader';
+// import ClientArea from './modules/ClientArea';
 
-import MyReactComponent from './modules/MyReactComponent';
+// import MyReactComponent from './modules/MyReactComponent';
 
 
 const root = createRoot(document.getElementById("my-react-example"))
-root.render(<MyReactComponent />);
+// root.render(<MyReactComponent />);
 
-new StickyHeader()
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
-new MobileMenu();
-new ClientArea();
+// new StickyHeader()
+// new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+// new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
+// new MobileMenu();
+// new ClientArea();
 
-let modal
+// let modal
 
-document.querySelectorAll(".open-modal").forEach(el => {
-  el.addEventListener("click", e => {
-    e.preventDefault();
-    if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
-        modal = new x.default();
-        setTimeout(() => modal.openModal(), 20)
-      }).catch(() => console.log("Файлыг дуудахад алдаа гарлаа"))
-    } else {
-      modal.openModal();
-    }
-  })
-})
+// document.querySelectorAll(".open-modal").forEach(el => {
+//   el.addEventListener("click", e => {
+//     e.preventDefault();
+//     if (typeof modal == "undefined") {
+//       import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+//         modal = new x.default();
+//         setTimeout(() => modal.openModal(), 20)
+//       }).catch(() => console.log("Файлыг дуудахад алдаа гарлаа"))
+//     } else {
+//       modal.openModal();
+//     }
+//   })
+// })
 
-if (module.hot) {
-  module.hot.accept()
-}
+// if (module.hot) {
+//   module.hot.accept()
+// }
