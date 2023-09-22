3ac2ab8090003fe9593104ef73fa11e8a64b0977
sukhbat98 <svhbat98@gmail.com>
2023-09-22 14:52:38 +0800

    header section enhanced

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,18 +6,15 @@ import 'lazysizes'
 import '../styles/styles.css';
 
 import MobileMenu from './modules/MobileMenu';
-// import RevealOnScroll from './modules/RevealOnScroll';
-// import StickyHeader from './modules/StickyHeader';
-// import ClientArea from './modules/ClientArea';
+import StickyHeader from './modules/StickyHeader';
 
 // import MyReactComponent from './modules/MyReactComponent';
-
 
 // const root = createRoot(document.getElementById("my-react-example"))
 // root.render(<MyReactComponent />);
 
 new MobileMenu();
-// new StickyHeader()
+new StickyHeader()
 // new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 // new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
 // new ClientArea();
--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -3,8 +3,7 @@ import debounce from 'lodash/debounce';
 
 class StickyHeader {
   constructor() {
-    this.siteHeader = document.querySelector(".site-header");
-    this.pageSections = document.querySelectorAll(".page-section")
+    this.siteHeader = document.querySelector(".navbar");
     this.browserHeight = window.innerHeight;
     this.previousScrollY = window.scrollY;
     this.events();
@@ -13,7 +12,6 @@ class StickyHeader {
   events() {
     window.addEventListener("scroll", throttle(() => this.runOnScroll(),  200))
     window.addEventListener("resize", debounce(() => {
-      console.log("checking")
       this.browserHeight = window.innerHeight
     }, 300))
   }
@@ -22,13 +20,11 @@ class StickyHeader {
     this.checkScrollDirection();
 
     if (window.scrollY > 60) {
-      this.siteHeader.classList.add("site-header--dark")
+      this.siteHeader.classList.add("navbar__header-dark")
     } else {
-      this.siteHeader.classList.remove("site-header--dark")
+      this.siteHeader.classList.remove("navbar__header-dark")
     }
 
-    // page section хэсэг
-    this.pageSections.forEach(el => this.calcSection(el))
   }
 
   checkScrollDirection() {
@@ -39,23 +35,6 @@ class StickyHeader {
     }
 
     this.previousScrollY = window.scrollY;
-  }
-
-  calcSection(el) {
-    if (window.scrollY + window.innerHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
-      let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight) * 100;
-      if (
-        scrollPercent < 18 &&
-        scrollPercent > -0.1 &&
-        this.scrollDirection == "down" ||
-        scrollPercent < 33 &&
-        this.scrollDirection == "up"
-      ) {
-        let matchingLink = el.getAttribute("data-matching-link");
-        document.querySelectorAll(`.primary-nav a:not(${matchingLink})`).forEach(el => el.classList.remove("is-current-link"));
-        document.querySelector(matchingLink).classList.add("is-current-link");
-      }
-    }
   }
 };
 
